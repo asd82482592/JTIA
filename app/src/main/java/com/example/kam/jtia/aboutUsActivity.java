60608ce94d04458c62389f4d786d36ba7605a67e
Kam <kam@Kamde-MacBook-Pro.local>
2017-10-09 10:14:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kam/jtia/aboutUsActivity.java
@@ -0,0 +1,22 @@
+package com.example.kam.jtia;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class aboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+
+
+
+
+    }
+}
